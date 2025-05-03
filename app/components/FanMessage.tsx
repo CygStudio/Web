679379b4f5aebfb0ab0ec4cd4169ea16748d1504
SidStraw <j80304xz@gmail.com>
2025-05-03 15:52:09 +0800

    feat: 更新 FanMessage API

--- app/components/FanMessage.tsx
+++ app/components/FanMessage.tsx
@@ -2,20 +2,24 @@ import InfiniteMarquee from '@/components/InfiniteMarquee'
 import StrokeText from '@/components/StrokeText'
 
 export default async function Home() {
-  const items = await fetch('https://cygstudio.github.io/asset/message.json').then(res =>
-    res.json()
-  )
+  const items = await fetch('https://cygstudio.github.io/asset/message')
+    .then(res => res.json())
+    .catch(() => null)
+
+  if (!items) return null
 
   return (
     <main className="flex flex-col lg:flex-row h-[200vh] lg:h-screen bg-[linear-gradient(to_bottom,#8d2a34_0%,#d16266_25%,#e7997c_50%,#d16266_75%,#8d2a34_100%)]">
-      <div className="flex-[2] relative bg-cover bg-center bg-no-repeat min-h-[100vh]" style={{ backgroundImage: 'url(/images/18a0793e589907a3.webp)' }}>
+      <div
+        className="flex-[2] relative bg-cover bg-center bg-no-repeat min-h-[100vh]"
+        style={{ backgroundImage: 'url(/images/18a0793e589907a3.webp)' }}>
         <h2 className="text-4xl font-bold text-white mt-8 ml-8">
           <StrokeText text="婚叫們的祝福" />
         </h2>
       </div>
 
       {/* 跑馬燈區域 */}
-      <InfiniteMarquee className='flex-1 lg:min-w-[680px]' items={items} />
+      <InfiniteMarquee className="flex-1 lg:min-w-[680px]" items={items} />
     </main>
   )
 }
