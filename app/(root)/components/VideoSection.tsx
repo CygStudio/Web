3ce6ebde40c304389671cf70a84de6fe69d64618
SidStraw <j80304xz@gmail.com>
2025-05-18 03:51:23 +0800

    feat: 影片改吃 API

--- app/(root)/components/VideoSection.tsx
+++ app/(root)/components/VideoSection.tsx
@@ -1,15 +1,32 @@
 import StrokeText from '@/components/StrokeText'
 
-export default function VideoSection() {
+type VideoItem = { title: string; url: string }
+type Common = {
+  videoList: VideoItem[]
+}
+
+export default async function VideoSectionList() {
+  const items: Common = await fetch('https://cygstudio.github.io/asset/common')
+    .then(res => res.json())
+    .catch(() => {
+      return { videoList: [] }
+    })
+
+  return items.videoList.map((item, index) => (
+    <VideoSection key={index} videoItem={item} />
+  ))
+}
+
+const VideoSection = ({ videoItem }: { videoItem: VideoItem }) => {
   return (
-    <section id="video" className='pt-5'>
+    <section id="video" className="pt-5">
       <h2 className="font-bold mb-5 text-3xl text-center">
-        <StrokeText text="慶生影片" />
+        <StrokeText text={videoItem.title} />
       </h2>
       <div className="max-w-[720px] mx-auto">
         <div className="iframe-container mx-auto">
           <iframe
-            src="https://www.youtube.com/embed/E739HotYZ4k"
+            src={videoItem.url}
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
